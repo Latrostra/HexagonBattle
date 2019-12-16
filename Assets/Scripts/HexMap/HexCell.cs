8a4162a7923075552d59e184b68bb736befa6a6b
Latrostra <43313391+Latrostra@users.noreply.github.com>
2019-12-16 14:06:19 +0100

    WorkingGrid

--- Assets/Scripts/HexMap/HexCell.cs
+++ Assets/Scripts/HexMap/HexCell.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshCollider), typeof(MeshRenderer))]
+
 public class HexCell : MonoBehaviour
 {
-
+    public HexCoordinates coordinates;
 }
--- Assets/Scripts/HexMap/HexGrid.cs
+++ Assets/Scripts/HexMap/HexGrid.cs
@@ -12,12 +12,13 @@ public class HexGrid : MonoBehaviour
     [SerializeField] private HexCell _cellPrefab;
 
     private HexCell[] cells;
-
-    Canvas gridCanvas;
+    private Canvas gridCanvas;
+    private HexMesh hexMesh;
 
     private void Awake()
     {
         gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
         cells = new HexCell[_width * _height];
 
         for(int z = 0, i = 0; z < _height; z++) {
@@ -27,18 +28,42 @@ public class HexGrid : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            HandleInput();
+        }
+    }
+    private void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            TouchCell(hit.point);
+        }
+    }
+    private void TouchCell(Vector3 position)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPostion(position);
+        Debug.Log("touched at " + coordinates.ToString());
+    }
     private void CreateCell(int x, int z, int i)
     {
-        Vector3 position = new Vector3(x * 10f, 0f, z * 10f);
-
+        Vector3 position = new Vector3((x + z * 0.5f - z / 2) * (HexMetric.innerRadius * 2f), 0f, z * HexMetric.outerRadius * 1.5f);
         HexCell cell = Instantiate<HexCell>(_cellPrefab);
         cells[i] = cell;
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
 
         Text label = Instantiate<Text>(_cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
         label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
-        label.text = x.ToString() + "\n" + z.ToString(); 
+        label.text = cell.coordinates.ToStringOnSeparateLines();
     }
 }
