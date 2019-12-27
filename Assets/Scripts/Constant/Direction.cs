329196a67f43b0e19db330fd0ec10f8b70bb9cb5
Latrostra <43313391+Latrostra@users.noreply.github.com>
2019-12-27 23:36:09 +0100

    Grid

--- Assets/Scripts/Constant/Direction.cs
+++ Assets/Scripts/Constant/Direction.cs
@@ -11,4 +11,10 @@ public static class HexDirectionExtension {
     public static HexDirection Opposite(this HexDirection direction) {
         return (int)direction < 3 ? (direction + 3) : (direction - 3);
     }
+    public static HexDirection Previous(this HexDirection direction) {
+        return direction == HexDirection.NE ? HexDirection.NW : (direction - 1);
+    }
+    public static HexDirection Next(this HexDirection direction) {
+        return direction == HexDirection.NW ? HexDirection.NE : (direction + 1);
+    }
 }
--- Assets/Scripts/HexMap/HexMesh.cs
+++ Assets/Scripts/HexMap/HexMesh.cs
@@ -11,8 +11,6 @@ public class HexMesh : MonoBehaviour
     private List<int> triangles;
     private MeshCollider meshCollider;
     private List<Color> colors;
-
-
     private void Awake()
     {
         meshCollider = gameObject.AddComponent<MeshCollider>();
@@ -38,26 +36,45 @@ public class HexMesh : MonoBehaviour
         hexMesh.RecalculateNormals();
         meshCollider.sharedMesh = hexMesh;
     }
-
-    private void Triangulate(HexCell cell)
-    {
-        Vector3 center = cell.transform.localPosition;
-        for(int i = 0; i < 6; i++) {
-            AddTriangle(center, 
-            center + HexMetric.corners[i], 
-            center + HexMetric.corners[i + 1]);
-            
-        AddTriangleColor(cell.color);
+    private void Triangulate(HexCell cell) {
+        for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+            Triangulate(d, cell);
         }
     }
+    private void Triangulate(HexDirection direction, HexCell cell) {
+        Vector3 center = cell.transform.localPosition;
+        Vector3 v1 = center + HexMetric.GetFirstSolidCorner(direction);
+        Vector3 v2 = center + HexMetric.GetSecondSolidCorner(direction);
 
-    private void AddTriangleColor(Color color)
+        AddTriangle(center, v1, v2);
+        AddTriangleColor(cell.color);
+        if (direction <= HexDirection.SE) {
+            TriangulateConnection(direction, cell, v1, v2);
+        }
+    }
+    private void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2) {
+
+        HexCell neighbour = cell.GetNeighbour(direction) ?? cell;
+        Vector3 bridge = HexMetric.GetBridge(direction);
+        Vector3 v3 = v1 + bridge;
+        Vector3 v4 = v2 + bridge;
+
+        AddQuad(v1, v2, v3, v4);
+        AddQuadColor(cell.color, neighbour.color);
+    }
+    private void AddTriangleColor(Color c1)
     {
-        colors.Add(color);
-        colors.Add(color);
-        colors.Add(color);
+        colors.Add(c1);
+        colors.Add(c1);
+        colors.Add(c1);
     }
 
+    private void AddTriangleColor(Color c1, Color c2, Color c3)
+    {
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c3);
+    }
     private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
     {
         int vertexIndex = vertices.Count;
@@ -67,5 +84,24 @@ public class HexMesh : MonoBehaviour
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
+    }
+    private void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4) {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        vertices.Add(v4);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 3);
+    }
+    private void AddQuadColor(Color c1, Color c2) {
+        colors.Add(c1);
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c2);
     }
 }
